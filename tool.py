53cebe7414344425bbaba126fc4ae8df7113b159
Illidanz <illidanrulez@gmail.com>
2020-04-18 15:55:40 +0200

    Generate font_data.bin file

--- tool.py
+++ tool.py
@@ -3,7 +3,7 @@ import click
 import game
 from hacktools import common, nds, nitro
 
-version = "0.3.0"
+version = "0.3.1"
 romfile = "data/mamoru.nds"
 rompatch = "data/mamoru_patched.nds"
 infolder = "data/extract/"
@@ -37,12 +37,18 @@ def repack(no_rom, mtrans, img):
     if all or mtrans:
         common.copyFile(mtransbin, mtransbinout)
         common.copyFile(mtransparm, mtransparmout)
+        fontfiles = []
+        fontfile = "data/replace/data/FONT/LC08.NFTR"
+        fontfiles.append(fontfile if os.path.isfile(fontfile) else fontfile.replace("replace/", "extract/"))
+        fontfile = "data/replace/data/FONT/LC10.NFTR"
+        fontfiles.append(fontfile if os.path.isfile(fontfile) else fontfile.replace("replace/", "extract/"))
+        nitro.extractFontData(fontfiles, "data/font_data.bin")
+        common.armipsPatch("bin_patch.asm")
     if all or img:
         nitro.repackIMG("data/work_IMG/", "data/extract/data/", "data/repack/data/", [".NCGR", ".nbfs"], game.readImage, game.writeImage)
     if not no_rom:
         if os.path.isdir(replacefolder):
             common.mergeFolder(replacefolder, outfolder)
-        common.armipsPatch("bin_patch.asm")
         nds.editBannerTitle(bannerfile, "I will protect you.\nIDEA FACTORY")
         nds.repackRom(romfile, rompatch, outfolder, patchfile)
 
